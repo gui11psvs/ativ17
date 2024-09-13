66fc1b3f60d0594be40c6edb3a5217ae272fb96b
guilhermelkx7@gmail.com <guilhermelkx7@gmail.com>
2024-09-13 11:30:19 -0300

    atividade17

---
+++ atividade_17.py
@@ -0,0 +1,12 @@
+# Crie um programa que receba um ano e verifique se ele é bissexto. Um ano é bissexto se for divisível por 4, exceto se for divisível por 100 e não por 400.
+def eh_bissexto(ano):
+    if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+        return True
+    else:
+        return False
+
+ano = int(input("Digite um ano: "))
+if eh_bissexto(ano):
+    print(f"{ano} é um ano bissexto.")
+else:
+    print(f"{ano} não é um ano bissexto.")
